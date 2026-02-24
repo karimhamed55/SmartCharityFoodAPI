a24d7b046e19cb4fab8242607d9bbdcc985e71d8
mohamed <mohamedtarek12633@gmail.com>
2026-02-24 23:38:21 +0200

    Add Food Donations feature with CRUD operations

---
+++ DTO/FoodDonationCreateDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace CharityApi.DTO
+{
+    public class FoodDonationCreateDTO
+    {
+        public int DonorId { get; set; }
+
+        public string FoodType { get; set; }
+
+        public int Quantity { get; set; }
+
+        public DateTime ExpiryDate { get; set; }
+    }
+}
---
+++ Models/FoodDonation.cs
@@ -0,0 +1,27 @@
+﻿using charityAPI.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CharityApi.Models
+{
+    public class FoodDonation
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int DonorId { get; set; }
+
+        [ForeignKey("DonorId")]
+        public Donor Donor { get; set; }
+
+        [Required]
+        public string FoodType { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        public DateTime ExpiryDate { get; set; }
+
+        public string Status { get; set; } = "Available";
+    }
+}
